7f23132dc05624b212604c84eb413793eb00cf8c
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-25 01:35:26 +0700

    Init table of contents

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Defining Your Own Python Function
 
 - Reference: https://realpython.com/defining-your-own-python-function
+
+- Run script
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,40 @@
+if __name__ == "__main__":
+  # Functions in Python
+
+  # The Importance of Python Functions
+  ## Abstraction and Reusability
+  ## Modularity
+  ## Namespace Separation
+
+  # Function Calls and Definition
+
+  # Argument Passing
+  ## Positional Arguments
+  ## Keyword Arguments
+  ## Default Parameters
+  ## Mutable Default Parameter Values
+  ## Pass-By-Value vs Pass-By-Reference in Pascal
+  ## Pass-By-Value vs Pass-By-Reference in Python
+  ## Argument Passing Summary
+  ## Side Effects
+
+  # The return Statement
+  ## Exiting a Function
+  ## Returning Data to the Caller
+  ## Revisiting Side Effects
+
+  # Variable-Length Argument Lists
+  ## Argument Tuple Packing
+  ## Argument Tuple Unpacking
+  ## Argument Dictionary Packing
+  ## Argument Dictionary Unpacking
+  ## Putting It All Together
+  ## Multiple Unpackings in a Python Function Call
+
+  # Keyword-Only Arguments
+
+  # Positional-Only Arguments
+
+  # Docstrings
+
+  # Python Function Annotations
