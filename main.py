c619f53754f7f51a43465fa06834d7fc1ac70289
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-25 02:22:18 +0700

    Functions in Python

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 if __name__ == "__main__":
   # Functions in Python
 
+  s = "foobar"
+  print(s)
+  print(id(s))
+
+  a = ["foo", "bar", "baz", "qux"]
+  print(a)
+  print(len(a))
+
+  print(any([False, False, False]))
+  print(any([False, True, False]))
+
+  print(any(["bar" == "baz", len("foo") == 4, "qux" in {"foo", "bar", "baz"}]))
+  print(any(["bar" == "baz", len("foo") == 3, "qux" in {"foo", "bar", "baz"}]))
+
   # The Importance of Python Functions
   ## Abstraction and Reusability
   ## Modularity
